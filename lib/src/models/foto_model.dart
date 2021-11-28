25297d70eea658a69895d65a5a2b78238c4e350d
JuanD4549 <juandiego402@gmail.com>
2021-11-28 12:03:28 -0500

    Update foto_model.dart
    ASDAS

---
+++ lib/src/models/foto_model.dart
@@ -0,0 +1,15 @@
+import 'package:meta/meta.dart';
+
+class Prueba extends StatefulWidget {
+  Prueba({Key? key}) : super(key: key);
+
+  @override
+  _PruebaState createState() => _PruebaState();
+}
+
+class _PruebaState extends State<Prueba> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
